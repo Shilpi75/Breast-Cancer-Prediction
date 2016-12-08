7a6d70d1107ec3c2077d6a7dabde725fb4c04c12
Shilpi Gupta <shilpi.c75@gmail.com>
2016-12-08 16:43:21 +0530

    Fuzzy c means clustering + fuzzy knn classifier

---
+++ Model6.r
@@ -0,0 +1,55 @@
+setwd("/media/shilpi/New Volume/ML/r/breast_cancer");
+data=read.csv("breastcancer.csv")
+data
+str(data)
+data.names();
+summary(data)
+
+
+#PRE PROCESSING
+data$Clump_Thickness=as.numeric(data$Clump_Thickness)
+data$Uniformity_CellSize=as.numeric(data$Uniformity_CellSize)
+data$Uniformity_CellShape=as.numeric(data$Uniformity_CellShape)
+data$Marginal_Adhesion=as.numeric(data$Marginal_Adhesion)
+data$Single_Epithelial_cellSize=as.numeric(data$Single_Epithelial_cellSize)
+data$BareNuclei=as.numeric(data$BareNuclei)
+data$Bland_Chromatin=as.numeric(data$Bland_Chromatin)
+data$Normal_Nucleoli=as.numeric(data$Normal_Nucleoli)
+data$Mitoses=as.numeric(data$Mitoses)
+data$Class=as.factor(data$Class)
+
+
+data[is.na(data$Clump_Thickness)]<-mean(data$Clump_Thickness,na.rm=TRUE)
+data[is.na(data$Uniformity_CellSize)]<-mean(data$Uniformity_CellSize,na.rm = TRUE)
+data[is.na(data$Uniformity_CellShape)]<-mean(data$Uniformity_CellShape,na.rm = TRUE)
+data[is.na(data$Marginal_Adhesion)]<-mean(data$Marginal_Adhesion,na.rm = TRUE)
+data[is.na(data$Single_Epithelial_cellSize)]<-mean(data$Single_Epithelial_cellSize,na.rm = TRUE)
+data[is.na(data$BareNuclei)]<-mean(data$BareNuclei,na.rm = TRUE)
+data[is.na(data$Bland_Chromatin)]<-mean(data$Bland_Chromatin,na.rm = TRUE)
+data[is.na(data$Normal_Nucleoli)]<-mean(data$Normal_Nucleoli,na.rm = TRUE)
+data[is.na(data$Mitoses)]<-mean(data$Mitoses,na.rm = TRUE)
+
+summary(data)
+
+set.seed(100)
+
+malignantdata=subset(data,Class==2)
+benigndata=subset(data,Class==4)
+str(malignantdata)
+
+k=2;
+library("fclust")
+KMC = FKM(malignantdata[ ,3:11], k= 2,m=2)
+KMC$clus[ ,1]
+
+
+
+
+malignantdata$Class=KMC$clus[ ,1]
+benigndata$Class=3
+
+
+data<-rbind(malignantdata,benigndata)
+write.csv(data, file = "BreastCancer3.csv",row.names = TRUE)
+
+#Run fuzzy knn python code on BreastCancer3.csv
